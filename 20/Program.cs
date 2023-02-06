953c495d18319dcc36305cb0db86d60a0f15ed4d
EkaterinaFedotova <katyaF85@yandex.ru>
2023-02-06 19:46:35 +0300

    Tasks 17, 18, 20, 22

--- 20/Program.cs
+++ 20/Program.cs
@@ -6,3 +6,16 @@
 // A(7, -5); B(1, -1)-> 7,21
 //sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
 //double s = Math.Sqrt(...);
+
+Console.Clear();
+Console.Write("Введите X1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+
+double s = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2));
+Console.WriteLine($"s = {s:f3}");
