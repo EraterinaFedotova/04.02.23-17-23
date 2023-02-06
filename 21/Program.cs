1d929e8a153d6667f14cb430172ab4524e880f7f
EkaterinaFedotova <katyaF85@yandex.ru>
2023-02-06 21:16:07 +0300

    Task 21

--- 21/Program.cs
+++ 21/Program.cs
@@ -4,3 +4,22 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.Write("Введите X1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите C1: ");
+double c1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите X2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.Write("Введите C2: ");
+double c2 = double.Parse(Console.ReadLine());
+
+
+double s = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(c1-c2, 2));
+Console.WriteLine($"s = {s:f2}");
